cf6c4edd33ce7cbe779971394cdf5ddeb361ca5c
GitLeeGH <tpwhd669@naver.com>
2023-10-15 17:16:14 +0900

    프로젝트 올리기

---
+++ src/main/java/com/dogwalk/dogwalking/controller/HomeController.java
@@ -0,0 +1,45 @@
+package com.dogwalk.dogwalking.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        System.out.println("index");
+        return "home";
+    }
+
+    @RequestMapping("/home")
+    public String home() {
+        System.out.println("home");
+        return "home";
+    }
+
+    @RequestMapping("about")
+    public String about() {
+        System.out.println("about");
+        return "about";
+    }
+
+    @RequestMapping("contact")
+    public String contact() {
+        System.out.println("contact");
+        return "contact";
+    }
+
+    @RequestMapping("courses")
+    public String courses() {
+        System.out.println("courses");
+        return "courses";
+    }
+
+    @RequestMapping("teachers")
+    public String teachers() {
+        System.out.println("teachers");
+        return "teachers";
+    }
+
+}
